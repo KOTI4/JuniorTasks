44b9126b068f6dec256374503b0093756fc2dfba
KOTI4 <lubozhevkoti4@gmail.com>
2022-06-21 21:39:38 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            int userScore;
+            float distanceFromPoint;
+            char teamSymbol;
+            double timeFromLastDash;
+            string messageInTextBox;
+            bool isDead;
+            int mobHealth;
+            float swordDamage;
+            bool isEnemy;
+        }
+    }
+}

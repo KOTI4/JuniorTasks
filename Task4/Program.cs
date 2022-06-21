98e9670842076052053cb95de933e11f95ecfdc2
KOTI4 <lubozhevkoti4@gmail.com>
2022-06-21 22:30:34 +0500

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите вашу фамилию");
+            var surname = Console.ReadLine();
+
+            Console.WriteLine("Укажите вас возвраст");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Укажите ваш знак зодиака");
+            var zodiacSign = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут {0} {1}. Вам {2}. Ваш знак зодиака - это {3}", surname, name, age, zodiacSign);
+            Console.ReadLine();
+        }
+    }
+}

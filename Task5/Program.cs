347c1710dff4e856efa602a9e95b26e8b7a432a6
KOTI4 <lubozhevkoti4@gmail.com>
2022-06-22 13:51:24 +0500

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Куджо";
+            string surname = "Джотаро";
+
+            Console.WriteLine("Имя - {0}. Фамилия - {1}.", name, surname);
+
+            string temp = name;
+            name = surname;
+            surname = temp;
+
+            Console.WriteLine("Имя - {0}. Фамилия - {1}.", name, surname);
+
+            Console.ReadLine();
+        }
+    }
+}

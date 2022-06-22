57ef30941d9aa936f145a695c5cd01b2951df0e8
KOTI4 <lubozhevkoti4@gmail.com>
2022-06-22 21:23:47 +0500

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float priceForCrystalInGold = 3.5f;
+
+            Console.WriteLine("Ваше количество золота?");
+            float playerGold = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сколько кристалов вы бы хотели приобрести по цене один кристал за {0} золотых?", 
+                priceForCrystalInGold);
+            int boughtCrystalsCount = int.Parse(Console.ReadLine());
+
+            float playerGoldAfterBuy = playerGold - boughtCrystalsCount * priceForCrystalInGold;
+
+            Console.WriteLine("Баланс кристалов {0}, баланс золотых {1}", boughtCrystalsCount, playerGoldAfterBuy);
+            Console.ReadLine();
+        }
+    }
+}

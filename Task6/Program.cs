9508adebea2e9c99a4a3068946fa41092a83eb0e
KOTI4 <lubozhevkoti4@gmail.com>
2022-06-22 15:03:55 +0500

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPicturesInRow = 3;
+            int totalPicturesCountInAlbum = 52;
+
+            int numberCompletedRows = totalPicturesCountInAlbum / numberPicturesInRow;
+            int numberPicturesBeyondMeasure = totalPicturesCountInAlbum % numberPicturesInRow;
+
+            Console.WriteLine("Количество заполненных рядов {0}, количество картинок сверх меры {1}",
+                numberCompletedRows, numberPicturesBeyondMeasure);
+            Console.ReadLine();
+        }
+    }
+}
